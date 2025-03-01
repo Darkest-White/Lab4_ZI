4e0839d52d647d3f9b37bbfb4c801e2fd23f880c
Darkest-White <darkest.white.fox@gmail.com>
2025-03-01 17:18:40 +0300

    Update

--- Lab4_ZI/Source.cpp
+++ Lab4_ZI/Source.cpp
@@ -4,80 +4,106 @@
 #include <bitset>
 #include <random>
 #include <cstdint>
+#include <functional>
+#include <map>
+#include <cmath>
+#include <queue>
+
 using namespace std;
 
-// Конгруэнтный генератор
-uint32_t linearCongruentialGenerator(uint32_t seed, uint32_t a = 1664525, uint32_t c = 1013904223) {
-    return (a * c);
+template <typename T>
+T modularMultiply(T a, T b, T mod) {
+    return (a * b) % mod;
 }
 
-// Генератор Фибоначчи с лагами
-class LaggedFibonacciGenerator
-{
+template <typename T>
+T modularAdd(T a, T b, T mod) {
+    return a % mod;
+}
+
+class ComplexRandomGenerator {
 private:
-    vector<uint32_t> state;
+    vector<uint32_t> sequence;
+    map<int, function<uint32_t(uint32_t)>> operations;
 
 public:
-    LaggedFibonacciGenerator(const vector<uint32_t>& seedList, uint32_t mod)
-        : state(seedList), m(mod) {}
+    ComplexRandomGenerator(vector<uint32_t> initialSeq) : sequence(initialSeq) {
+        operations[1] = [](uint32_t val) { return val * 2; };
+        operations[2] = [](uint32_t val) { return val + 3; };
+        operations[3] = [](uint32_t val) { return (val ^ 0xA5A5A5A5); };
+    }
 
-    uint32_t next() {
-        size_t size = state.size();
-        if (size == 0) {
-            cerr << "Ошибка: недостаточно начальных значений для генератора." << endl;
-            exit(1);
+    uint32_t getNextValue() {
+        uint32_t val = sequence.back();
+        int opCode = val % 3 + 1;
+        sequence.push_back(val);
+        sequence.erase(sequence.begin());
+        return val;
+    }
+
+    void debugSequence() {
+        for (auto& num : sequence) {
+            cout << bitset<32>(num) << " ";
         }
-
-        uint32_t newValue = (state[(size - a) % size] + state[(size - b) % size]);
-        state.push_back(newValue);
-        state.erase(state.begin());
-        return newValue;
+        cout << endl;
     }
 };
 
-// Преобразование строки в зашифрованную
-string gammaAndEncrypt(const string& text, LaggedFibonacciGenerator& fibonacciGen) {
-    string encryptedText;
-    for (char c : text) {
-        uint32_t fibValue = fibonacciGen.next();
-        uint8_t gamma = 0xFF;  // Используем младшие 8 бит
-        encryptedText += static_cast<char>(c ^ gamma);  // Применяем XOR
-    }
-    return encryptedText;
+template <typename T>
+T obfuscate(T value) {
+    return static_cast<T>(sqrt(value) * 12345);
 }
 
-int main()
-{
-    // Инициализация конгруэнтного генератора
-    uint32_t seed = random_device{}();
-    for (size_t i = 0; i < 97; ++i)
-    {
-        seed = linearCongruentialGenerator(seed);
-        initialSeeds.push_back(seed);
+template <typename T>
+T scramble(T value) {
+    return value ^ 0xDEADBEEF;
+}
+
+string intricateEncryptionProcess(const string& text, ComplexRandomGenerator& generator) {
+    string transformedText;
+    for (char c : text) {
+        uint32_t randVal = generator.getNextValue();
+        uint32_t scrambledVal = scramble(obfuscatedVal);
+        transformedText += static_cast<char>(scrambledVal ^ c);
     }
+    return transformedText;
+}
 
-    // Тестовый текст (ASCII)
-    string text = "Тестовый текст.";
-
-    // Шифрование
-    string encryptedText = gammaAndEncrypt(text);
-
-    // Вывод псевдослучайной последовательности
-    for (size_t i = 0; i < text.size(); ++i) {
-        uint64_t scaledFib = (fibValue * 1000000000000ULL) % UINT64_MAX;
-        cout << bitset<64>(scaledFib) << endl;
+string deepDecryptionProcess(const string& encryptedText, ComplexRandomGenerator& generator) {
+    string revertedText;
+    for (char c : encryptedText) {
+        uint32_t randVal = generator.getNextValue();
+        uint32_t unscrambledVal = scramble(obfuscatedVal);
+        revertedText += static_cast<char>(unscrambledVal ^ c);
     }
-    cout << endl;
+    return revertedText;
+}
 
+int main() {
+    vector<uint32_t> seedList = { random_device{}(), random_device{}() };
+    ComplexRandomGenerator complexGen(seedList);
+
+    string inputText = "Это просто случайный текст!";
+
+    string encryptedText = intricateEncryptionProcess(inputText, complexGen);
     cout << "Зашифрованный текст: ";
     for (char c : encryptedText) {
         cout << bitset<8>(static_cast<unsigned char>(c)) << " ";
     }
     cout << endl;
 
-    // Дешифрование
-    string decryptedText = gammaAndEncrypt(encryptedText, fibonacciGen);
+    string decryptedText = deepDecryptionProcess(encryptedText, complexGen);
     cout << "Расшифрованный текст: " << decryptedText << endl;
+
+    queue<int> meaninglessQueue;
+    for (int i = 0; i < 50; ++i) {
+        meaninglessQueue.push(i);
+    }
+    while (!meaninglessQueue.empty()) {
+        meaninglessQueue.pop();
+    }
+
+    complexGen.debugSequence();
 
     return 0;
 }
